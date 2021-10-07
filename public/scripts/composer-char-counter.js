b79ada676c5e1aa239b2eb25c479a234db5a3297
charuzain <charuabhizain@gmail.com>
2021-10-07 00:10:37 -0400

    Character counter implemented

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#tweet-text').on('input',function(){
+   let maxCharacter = 140;
+   let counter = maxCharacter-$(this).val().length;
+   let counterElement=$(this).next().find('.counter');
+   $(counterElement).val(`${counter}`)
+   if(counter>0){
+    $('.counter').css("color", "black")
+   }
+   else if(counter<0){$('.counter').css("color", "red")};
+  
+  })
+});
