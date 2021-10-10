6ca5aff35ac68ee91a6fcce1a99b73b825fb99c6
charuzain <charuabhizain@gmail.com>
2021-10-10 02:27:18 -0400

    code cleanup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
-  $('#tweet-text').on('input',function(){
-   let maxCharacter = 140;
-   let counter = maxCharacter-$(this).val().length;
-   let counterElement=$(this).next().find('.counter');
-   $(counterElement).val(`${counter}`)
-   if(counter<0){
-    $('.counter').addClass("redCounter")
-   }
-   else {
-     $('.counter').removeClass("redCounter")
-    };
+$(document).ready(function() {
+  $('#tweet-text').on('input',function() {
+    let maxCharacter = 140;
+    let counter = maxCharacter - $(this).val().length;
+    let counterElement = $(this).next().find('.counter');
+    $(counterElement).val(`${counter}`);
+    if (counter < 0) {
+      $('.counter').addClass("redCounter");
+    } else {
+      $('.counter').removeClass("redCounter");
+    }
   
-  })
+  });
 });
